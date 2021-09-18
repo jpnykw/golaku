41d9ec5a76d49e90239494aaf3da9255fc988e88
jpnykw <jpnykw@gmail.com>
2021-09-18 17:09:13 +0900

    :recycle: Fix indent spaces

--- tour/exercise-loops-and-functions.go
+++ tour/exercise-loops-and-functions.go
@@ -2,14 +2,14 @@ package main
 import "fmt"
 
 func Sqrt(x float64) (z float64) {
-	z = 1.0
-	for i := 0; i < 10; i++ {
-		z -= (z * z - x) / (2 * z)
-	}
-	return
+  z = 1.0
+  for i := 0; i < 10; i++ {
+    z -= (z * z - x) / (2 * z)
+  }
+  return
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+  fmt.Println(Sqrt(2))
 }
 
--- tour/exercise-maps.go
+++ tour/exercise-maps.go
@@ -1,20 +1,20 @@
 package main
 import (
-	"golang.org/x/tour/wc"
-	"strings"
+  "golang.org/x/tour/wc"
+  "strings"
 )
 
 type Res = map[string]int
 
 func WordCount(s string) (words Res) {
-	words = make(Res)
-	for _, word := range strings.Fields(s) {
-		words[word] += 1
-	}
-	return
+  words = make(Res)
+  for _, word := range strings.Fields(s) {
+    words[word] += 1
+  }
+  return
 }
 
 func main() {
-	wc.Test(WordCount)
+  wc.Test(WordCount)
 }
 
